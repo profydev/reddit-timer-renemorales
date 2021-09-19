de02d7a4d7ef60fc0d4449825d6fa467999b1792
Rene Morales <36004614+renemorales@users.noreply.github.com>
2021-09-19 12:29:35 -0500

    Header (#2) (#2)
    * Header (#2)
    
    - Reset default browser styles
    - Add global styles
    - Add router with routes / and /search
    - Add global fonts
    - Change app title and meta description
    - Add globalStyle & theme
    
    * - Add header component
    -  Add logo and links
    - Review the code
    - Fix merge conflicts
    - Change the URL to search link
    
    * Add new route
    
    Co-authored-by: Rene <rene.developer@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import GlobalStyle from './GlobalStyle';
+import theme from './theme';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Normalize />
+        <GlobalStyle />
+        <Switch>
+          <Route path="/search">Search</Route>
+          <Route path="/how-it-works">How it works</Route>
+          <Route path="/about">About</Route>
+          <Route>404 - Not Found</Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
