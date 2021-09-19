03dd49286461c15907738a9d413d5e79ed493510
Rene <rene.developer@gmail.com>
2021-09-19 12:26:54 -0500

    Add new route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Normalize } from 'styled-normalize';
-// import Header from "./Header";
 import GlobalStyle from './GlobalStyle';
 import theme from './theme';
 
@@ -13,8 +12,9 @@ function App() {
         <Normalize />
         <GlobalStyle />
         <Switch>
-          <Route path="/">Home</Route>
           <Route path="/search">Search</Route>
+          <Route path="/how-it-works">How it works</Route>
+          <Route path="/about">About</Route>
           <Route>404 - Not Found</Route>
         </Switch>
       </Router>
